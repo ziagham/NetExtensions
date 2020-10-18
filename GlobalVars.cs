0938417d33b74da92fd8348073f0d72f2308b008
Amin Ziagham <19279754+aminZiagham@users.noreply.github.com>
2020-10-19 01:37:40 +0200

    Add files via upload

---
+++ GlobalVars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class GlobalVars
+    {
+        /// <summary>
+        /// An enumeration of the types of masking styles for the Mask() extension method
+        /// of the string class.
+        /// </summary>
+        public enum MaskStyle
+        {
+            /// <summary>
+            /// Masks all characters within the masking region, regardless of type.
+            /// </summary>
+            All,
+
+            /// <summary>
+            /// Masks only alphabetic and numeric characters within the masking region.
+            /// </summary>
+            AlphaNumericOnly,
+        }
+    }
+}
